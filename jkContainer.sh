945f13fd1aaef7cb7cce28729e2564b7565b96a0
d3jk <denniskennedy113@gmail.com>
2022-03-07 20:12:16 -0600

    Add some cgroups

--- jkContainer.sh
+++ jkContainer.sh
@@ -3,6 +3,8 @@
 rootfs="minirootfs"
 sourcefile=".sourcefile"
 
+trap "echo 'Thanks, come again!'" EXIT
+
 # check if source file already exists
 if [[ -f "${rootfs}/${sourcefile}" ]]; then
     echo "" > ${rootfs}/${sourcefile}
@@ -12,6 +14,17 @@ fi
 cmds=(\
     "mount -t proc proc /proc" \
     "mount -t sysfs sysfs /sys" \
+    "mount -t tmpfs cgroup_root /sys/fs/cgroup" \
+    "mkdir /sys/fs/cgroup/memory" \
+    "mkdir /sys/fs/cgroup/blkio" \
+    "mkdir /sys/fs/cgroup/pids" \
+    "mkdir /sys/fs/cgroup/rdma" \
+    "mkdir /sys/fs/cgroup/freezer" \
+    "mount -t cgroup -o memory cgroup_memory /sys/fs/cgroup/memory/" \
+    "mount -t cgroup -o blkio cgroup_blkio /sys/fs/cgroup/blkio/" \
+    "mount -t cgroup -o pids cgroup_pids /sys/fs/cgroup/pids/" \
+    "mount -t cgroup -o rdma cgroup_rdma /sys/fs/cgroup/rdma/" \
+    "mount -t cgroup -o freezer cgroup_freezer /sys/fs/cgroup/freezer/" \
     "hostname jkcontainer"
 )
 
@@ -27,6 +40,4 @@ unshare \
     --net \
     --cgroup \
     chroot ${rootfs} /bin/sh -c '. '"${sourcefile}"'; exec /bin/sh'
-
-
 
